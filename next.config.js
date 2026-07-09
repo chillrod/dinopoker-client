f5e838d154258b6fa4b25d078d728b26f8ad7aa9
chillrod <chillrod77@gmail.com>
2026-07-09 11:23:16 -0300

    feat: next update

--- next.config.js
+++ next.config.js
@@ -1,21 +1,6 @@
+const nextTranslate = require("next-translate-plugin");
+
 /** @type {import('next').NextConfig} */
-const { i18n } = require("./i18n");
-
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  i18n,
-};
-
-module.exports = {
-  ...nextConfig,
-};
-
-const nextTranslate = require("next-translate");
-
 module.exports = nextTranslate({
   reactStrictMode: true,
-  webpack: (config, { isServer, webpack }) => {
-    return config;
-  },
 });
